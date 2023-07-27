d56544621859f490a8ed98de627595732d22d655
bjjumba <ericjjumba5@gmail.com>
2023-07-27 15:32:37 +0300

    Handlecors mtd edit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,15 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', '*');
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 200);
+        } else {
+            $response = $next($request);
+        }
+
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 }
